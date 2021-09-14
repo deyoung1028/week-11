083dd22f3b1b28981be90b8c0d155333372d44b5
deyoung1028 <deyoung1028@yahoo.com>
2021-09-13 20:44:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+import Counter from './components/Counter'
+import IncdecCounter from './components/IncrementDecrementCounter'
+import AddSubtractCounter from './components/AddSubtractCounter';
 
-function App() {
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className = "header">APP</h1>
+      <Counter/>
+      <AddSubtractCounter />
+      <IncdecCounter/>
+
+    </>
+  )
+
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    ctr: state.counter
+  }
+}
+
+export default connect(mapStateToProps)(App)
---
+++ src/store/reducer.js
@@ -0,0 +1,33 @@
+
+const initialState = {
+    counter:0
+}
+
+const reducer = (state = initialState, action) => {
+     
+    if(action.type == 'INCREMENT') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }else if(action.type == 'DECREMENT') {
+        return {
+            ...state,
+            counter: state.counter -1
+        }
+    }else if(action.type == 'ON_SUBTRACT_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter - action.payload
+        }
+    }else if(action.type == 'ON_ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.payload
+        }
+    }
+
+    return state
+}
+
+export default reducer
